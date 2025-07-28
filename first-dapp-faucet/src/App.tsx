91d7122d085ec1f95fe5ea47efdb70351f8a2813
Sarthak Italiya <sarthak.italiya2005@gmail.com>
2025-07-28 17:01:16 +0530

    feat: implement routing and new components for send token functionality

--- first-dapp-faucet/src/App.tsx
+++ first-dapp-faucet/src/App.tsx
@@ -4,14 +4,12 @@ import {
 } from "@solana/wallet-adapter-react";
 import {
   WalletModalProvider,
-  WalletDisconnectButton,
-  WalletMultiButton,
 } from "@solana/wallet-adapter-react-ui";
 
 import "@solana/wallet-adapter-react-ui/styles.css";
-import Airdrop from "./Airdrop";
-import { ShowSolBalance } from "./ShowSolBalance";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import FaucetPage from "./components/FaucetPage";
+import SendTokensPage from "./components/SendTokensPage";
 
 function App() {
   const endpoint = import.meta.env.VITE_SOLANA_RPC_URL!;
@@ -19,26 +17,12 @@ function App() {
     <ConnectionProvider endpoint={endpoint}>
       <WalletProvider wallets={[]} autoConnect>
         <WalletModalProvider>
-          <div className="min-h-screen bg-[#1e1e1e] text-white flex flex-col items-center justify-center px-4 py-10">
-            <div className="w-full max-w-2xl bg-[#2a2a2a] rounded-md shadow-lg border border-gray-700">
-              <div className="flex flex-col items-center p-6 border-b border-gray-600">
-                <h1 className="text-2xl font-bold mb-1">💦 Sol Faucet 💦</h1>
-                <p className="text-sm text-gray-400 mb-4">
-                  This tool does <span className="text-red-500 font-semibold">NOT</span> give real $SOL or Solana tokens.
-                </p>
-                <div className="flex gap-2 sm:flex-row flex-col">
-                  <WalletMultiButton />
-                  <WalletDisconnectButton />
-                </div>
-              </div>
-              <main className="flex flex-col items-center p-6">
-                <h2 className="text-xl font-semibold text-center">Request Airdrop</h2>
-                <p className="text-sm text-gray-400 mb-4">Maximum of 2 requests every 8 hours</p>
-                <ShowSolBalance />
-                <Airdrop />
-              </main>
-            </div>
-          </div>
+          <Router>
+            <Routes>
+              <Route path="/" element={<FaucetPage />} />
+              <Route path="/sendtoken" element={<SendTokensPage />} />
+            </Routes>
+          </Router>
         </WalletModalProvider>
       </WalletProvider>
     </ConnectionProvider>
---
+++ first-dapp-faucet/src/components/FaucetPage.tsx
@@ -0,0 +1,45 @@
+import {
+  WalletDisconnectButton,
+  WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
+import { ShowSolBalance } from "./ShowSolBalance";
+import Airdrop from "./Airdrop";
+import { Link } from "react-router-dom";
+
+const FaucetPage = () => {
+  return (
+    <div className="min-h-screen bg-[#1e1e1e] text-white flex flex-col items-center justify-center px-4 py-10">
+      <div className="w-full max-w-2xl bg-[#2a2a2a] rounded-md shadow-lg border border-gray-700">
+        <div className="flex flex-col items-center p-6 border-b border-gray-600 relative">
+          <Link
+            to="/sendtoken"
+            className=" absolute left-5 top-5 bg-[#512DA8] hover:bg-[#6B5B9A] text-white text-sm p-2 rounded-md  cursor-pointer"
+          >
+            Go to Send Tokens
+          </Link>
+
+          <h1 className="text-2xl font-bold mb-1">💦 Sol Faucet 💦</h1>
+          <p className="text-sm text-gray-400 mb-4">
+            This tool does{" "}
+            <span className="text-red-500 font-semibold">NOT</span> give real
+            $SOL or Solana tokens.
+          </p>
+          <div className="flex gap-2 sm:flex-row flex-col">
+            <WalletMultiButton />
+            <WalletDisconnectButton />
+          </div>
+        </div>
+        <main className="flex flex-col items-center p-6">
+          <h2 className="text-xl font-semibold text-center">Request Airdrop</h2>
+          <p className="text-sm text-gray-400 mb-4">
+            Maximum of 2 requests every 8 hours
+          </p>
+          <ShowSolBalance />
+          <Airdrop />
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default FaucetPage;
