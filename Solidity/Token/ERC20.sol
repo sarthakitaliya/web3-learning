3028114f7e2af741c4e8504dbdced603516f8cb4
Sarthak Italiya <sarthak.italiya2005@gmail.com>
2025-08-05 01:43:59 +0530

    sol - tried ERC20

---
+++ Solidity/Token/ERC20.sol
@@ -0,0 +1,17 @@
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyToken is ERC20, Ownable {
+    constructor() ERC20("Catuu", "Cu") Ownable(msg.sender) {
+        _mint(msg.sender, 1000000000);
+    }
+
+    function _mintTo(address account, uint256 amount) public onlyOwner{
+        _mint(account, amount);
+    }
+}
